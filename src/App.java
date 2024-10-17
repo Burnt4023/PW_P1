eab9dd50df226e60b1cc6268cfa870ff1f5ca736
RafithA <rafael.molleja04@gmail.com>
2024-10-17 16:00:50 +0200

    Menu APP

--- src/App.java
+++ src/App.java
@@ -1,10 +1,35 @@
-import classes.*;
+
+import java.util.Scanner;
+
 
 public class App {
     public static void main(String[] args) throws Exception {
 
-        Material example;
+        // Iniciar el scanner.
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Hello, World!!");
+        while (true) {
+
+            System.err.println("--- GESTOR DE POLIDEPORTIVO ---\n");
+
+            System.err.println("1. Gestion de Usuarios");
+            System.err.println("2. Gestion de Reservas");
+            System.err.println("3. Gestion de Pistas");
+            System.err.println("4. Salir");
+
+            String option = scanner.nextLine();
+
+            // switch;
+
+
+
+        }
+
+
+
+
+
+
+
     }
 }
